3f9918931908e8004debcbb22491fdfb98a00716
baydouna <ali.baydoun@etu.univ-grenoble-alpes.fr>
2024-11-04 14:37:26 +0100

    added comments for prologue and epilogue in generator.cpp

--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -19,8 +19,9 @@ void CodeGenerator::generate(const ParseNode& parseTree, const map<string, strin
     for (const auto& child : parseTree.children) {
         if (child->type == NodeType::FUNCTION_DECL) {
             body += child->value.value() + ":\n";
-            body += "\tpush ebp\n";
-            body += "\tmov %ebp, %esp\n";
+            body += "\tpush ebp\n"; //Save the stack-frame base pointer (of the calling function).
+            body += "\tmov %ebp, %esp\n"; //; Set the stack-frame base pointer to be the current location on the stack.
+//            body += "\tsub %esp, N"; // The N immediate value is the number of bytes reserved on the stack for local use
 
             for (const auto& stmt : child->children) {
                 if (stmt->type == NodeType::STATEMENT) {
@@ -28,9 +29,9 @@ void CodeGenerator::generate(const ParseNode& parseTree, const map<string, strin
                 }
             }
 
-            body += "\tmov %esp, %ebp\n";
-            body += "\tpop ebp\n";
-            body += "\tret\n\n";
+            body += "\tmov %esp, %ebp\n"; // Put the stack pointer back where it was when this function was called.
+            body += "\tpop ebp\n"; // Restore the calling function's stack frame.
+            body += "\tret\n\n"; // Return to the calling function.
         }
     }
 
